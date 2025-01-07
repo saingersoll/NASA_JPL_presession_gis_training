650996c34323877f0735f9cb4ea74d61e2b890ff
saingersoll <sofia.ingersoll@Outlook.com>
2025-01-07 11:23:44 -0800

    ARSET spatial indicies for land and aquatic applications notes uploaded + some groundwork to make goggle earth engine (gee) accessible via python using an api

---
+++ ARSET/google_earth_engine_js/test_files/ARSET_gee_1.js
@@ -0,0 +1,8 @@
+var title = ui.Label({
+    value: 'Part 1: Exploring NDVI',
+    style:{
+    fontWeight: 'bold',
+    fontSize: '18px'
+    }});
+  title.style().set('position', 'top-center');
+  Map.add(title);
